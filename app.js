55311d362ca7dfac14281caad2ac4e4e26b11209
av308q <angelv1901@gmail.com>
2020-01-04 08:47:29 -0500

    setting up event listener for calc btn

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert(1);
+//listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+//Calculate Results 
+function calculateResults(e){
+  console.log('calculating...')
+
+  e.preventDefault();
+}
