21d68dd1fce989e12c94e987b73fdd113d059774
av308q <angelv1901@gmail.com>
2020-01-05 13:17:07 -0500

    created Vars  for loan calc

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 //Calculate Results 
 function calculateResults(e){
   console.log('calculating...')
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthyPayment = document.getElementById('monthy-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+
+
 
   e.preventDefault();
 }
